3d4092ad99ff71201e708df87de7d935dca5abe1
Sujan Subedi <153894689+sujansubedi89@users.noreply.github.com>
2026-06-04 19:36:57 +0545

    Clean up comments in settings.py
    Removed comments related to development settings.

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -16,8 +16,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('django-secret')
